f932b743359da121221ac7da1cb08ceb2bd8b8ca
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-10 01:43:04 -0700

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - Entry point
+*Use the function puts
+*You are not allowed to use printf
+*Your program should end with the value 0
+* Return: Always 0 (Success)
+*/
+
+in main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
+
