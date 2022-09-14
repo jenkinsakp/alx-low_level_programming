4533e04da420053af44286472d8ef4e37f387c1d
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-14 14:46:51 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,22 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
- * writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-	char akp[8] = "_putchar";
-	int count  = 0;
-	char checker;
-
-		while (count <= 8) 
-		{
-			checker = akp[count];
-			_putchar(checker);
-			count++;
-		}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 
-}
-
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
 }
