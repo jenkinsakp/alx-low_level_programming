89763deef20c9348fe67535a44a229c85e88012a
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-27 13:35:20 -0700

    Hello Puts correction  - Hell print

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,46 @@
 #include "main.h"
-/**memset fills a memory block with a constant byte
-*@s: address to memory block @b: char to be used
-*@n: number of bytes to be used
-* Return: pointer to the memory block
-* */
+#include <stdio.h>
 
-char *_memset(char *s, char b, unsigned int n) }
-	while (n)
-	{	
-		s[n-1] = b;
-		n--;
-	}
-	return (s);
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
