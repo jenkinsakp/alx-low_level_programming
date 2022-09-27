611a6978164319fa8287c86909168788a26ffbd0
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-27 08:09:37 -0700

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 
 /**
   * _memset - Fill memory with a constant byte
-  * @s: memory  to blocked
-  * @b: char to fill
+  * @s: memory area to fill
+  * @b: constant byte to fill
   * @n: bytes of memory area to fill
   *
-  * Return: the pointer to the  area block
+  * Return: the memory area filled
   */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int akp;
+	unsigned int a;
 
-	for (a = 0; akp < n; akp++)
+	for (a = 0; a < n; a++)
 	{
-		s[akp] = b;
+		s[a] = b;
 	}
 
 	return (s);
