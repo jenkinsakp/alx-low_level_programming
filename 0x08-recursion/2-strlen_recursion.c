cefa770f30a5a5b7fc9cf5e53d84406ab411ca02
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-29 15:47:45 -0700

    Tested

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,13 +14,9 @@
  */                                                                                                                                     
 int _strlen_recursion(char *s)                                                                                                          
 {                                                                                                                                       
-        int len = 0;                                                                                                                    
-                                                                                                                                        
-        if (*s)                                                                                                                         
+        int len = 0;                                                                                                                                                    if (*s)                                                                                                                        
         {                                                                                                                               
-                len++;                                                                                                                  
-                len += _strlen_recursion(s + 1);                                                                                        
+                len++;                                                                          len += _strlen_recursion(s + 1);                                                                                        
         }                                                                                                                               
-                                                                                                                                        
-        return (len);                                                                                                                   
+         return (len);                                                                                                                   
 }
