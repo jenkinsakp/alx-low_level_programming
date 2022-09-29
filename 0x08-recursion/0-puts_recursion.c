2fa4b4e775fdb9068c47b53e8a9d30630dbc2e88
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-29 15:43:58 -0700

    Tested

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,22 @@
+#include "main.h"
 
-/*                                                                                                                                      
- * File: 0-puts_recursion.c                                                                                                             
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _puts_recursion - Prints a string, followed by a new line.                                                                           
- * @s: The string to be printed.                                                                                                        
- */                                                                                                                                     
-void _puts_recursion(char *s)                                                                                                           
-{                                                                                                                                       
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
-        }                                                                                                                               
-                                                                                                                                        
-        else                                                                                                                            
-                _putchar('\n');                                                                                                         
+/**
+  * _puts_recursion - Print a string followed by a new line
+  * @s: the string to print
+  *
+  * Return: Nothing.
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
+
+                                                                                                
