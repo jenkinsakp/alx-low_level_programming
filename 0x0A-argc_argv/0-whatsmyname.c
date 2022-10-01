c668a72a46bb6b2825587639d0e622588afdf235
jenkinsakp <unuavworhoakpevwe@gmail.com>
2022-09-30 21:49:45 -0700

    majot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,19 @@
-#include "main.h"
-#include <stdio.h>
 
-/**
-  * main - Prints its name
-  * @argc: argument count
-  * @argv: argument vector
-  *
-  * Return: Always zero
-  */
-int main(int argc, char *argv[])
-{
-        (void) argc;
-        printf("%s\n", argv[0]);
+#include <stdio.h>                                                                                                                   
+#include "main.h"                                                                                                                    
 
-        return (0);
+/**                                                                                                                                  
+ * Display the name of the program                                                                                              
+ * @argc: Count arguments                                                                                                            
+ * @argv: Arguments                                                                                                                  
+ *                                                                                                                                   
+ * Return: Always 0 (Success)                                                                                                        
+ */                                                                                                                                  
+
+int main(int argc, char *argv[])                                                                                                     
+{                                                                                                                                    
+(void) argc;                                                                                                                         
+printf("%s\n", argv[0]);                                                                                                             
+
+return (0);                                                                                                                          
 }
-
