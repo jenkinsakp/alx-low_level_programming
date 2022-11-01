4e74db2f173f7f6847f01777e6cc5a94b4b8fbff
Akpevwe Jenkins U <34058396+jenkinsakp@users.noreply.github.com>
2022-11-02 00:37:19 +0400

    updated

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
+char *create_array(unsigned int, char);
+char *_strdup(char*);
+char *str_concat(char*, char*);
+int **alloc_grid(int, int);
+void free_grid(int**, int);
+char *argstostr(int, char **);
+
+#endif
